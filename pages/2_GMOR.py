09ffe2ce4db5176f5620f4bc6fb288ef6388de18
ricardoc1999duarte <64763278+ricardoc1999duarte@users.noreply.github.com>
2025-10-03 23:31:46 -0300

    Add files via upload

---
+++ pages/2_GMOR.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.set_page_config(page_title="GMOR", layout="wide")
+st.title("GMOR - Gerência de Monitoramento de Obras")
+st.write("Esta área é dedicada às análises e ferramentas da GMOR.")
---
+++ pages/3_GECIE.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.set_page_config(page_title="GECIE", layout="wide")
+st.title("GECIE - Gerência de Custos e Informações Estratégicas")
+st.write("Esta área é dedicada às análises e ferramentas da GECIE.")
