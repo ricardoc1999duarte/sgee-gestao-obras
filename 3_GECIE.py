41fd1661eac009ab7d69cf0be4b0da0ee44ed18a
ricardoc1999duarte <64763278+ricardoc1999duarte@users.noreply.github.com>
2025-10-03 23:30:03 -0300

    Delete 3_GECIE.py

--- 3_GECIE.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-st.set_page_config(page_title="GECIE", layout="wide")
-st.title("GECIE - Gerência de Custos e Informações Estratégicas")
-st.write("Esta área é dedicada às análises e ferramentas da GECIE.")
